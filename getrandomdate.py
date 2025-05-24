cdba8faf52c8a0bf46f2f664c5772dc197e66576
NatalieZawadi <nataliezawadiwesula@gmail.com>
2025-05-24 20:36:07 +0300

    get random date

--- getrandomdate.py
+++ getrandomdate.py
@@ -5,7 +5,7 @@ def getRandomDate(StartDate, endDate):
     print("Printing random date between", StartDate, "and", endDate)
     randomGenerator = random.random()
     dateFormat = "%m/%d/%Y"
-    startTime = time.mktime(time.strptime(StartDate, dateFormat"))
+    startTime = time.mktime(time.strptime(StartDate, dateFormat))
     endTime = time.mktime(time.strptime(endDate, dateFormat))
     randomTime = startTime + randomGenerator * (endTime - startTime)
     randomDate = time.strftime(dateFormat, time.localtime(randomTime))
